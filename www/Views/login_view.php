7c580b00b52e7d5823325e3389a61f5b21a14813
Chaochao <chaochao.zhou@hotmail.com>
2021-07-01 12:32:39 +0200

    View login

---
+++ www/Views/login_view.php
@@ -0,0 +1,10 @@
+<form>
+
+    <!-- logo -->
+
+    <label>Identifiant</label>
+    <input type="text" placeholder="Email Address" name="identifiant">
+    <label>Mot de passe</lable>
+    <input type="password" placeholder="Password" name="password">
+    
+</form>
