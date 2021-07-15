6356b64d3dd3ff1172ae0fe0d16e792a583a69af
ronantr <noob93500@gmail.com>
2021-07-15 10:45:57 -0700

    les commentaires ne s'ajoute pas a la bdd

--- www/Models/Commentaire.php
+++ www/Models/Commentaire.php
@@ -9,6 +9,8 @@ class Commentaire extends Database
 {
 	private $id=null;
 	protected $content;
+    protected $id_article;
+    protected $id_user;
 
 	public function __construct(){
 		parent::__construct();
@@ -46,13 +48,47 @@ class Commentaire extends Database
     {
         return $this->content;
     }
+
+    /**
+    * @param $id_article
+    */
+
+	public function setCommentaire_id_article($id_article){
+		$this->id_article = $id_article;
+	}
+    /**
+    * @return mixed
+    */
+
+    public function getId_article()
+    {
+        return $this->id_article;
+    }
+
+    /**
+    * @param $id_article
+    */
+
+	public function setCommentaire_id_user($id_user){
+		$this->id_user = $id_user;
+	}
+
+	/**
+    * @return mixed
+    */
+
+    public function getId_user()
+    {
+        return $this->id_user;
+    }    
+
     public function buildFormRegister(){ 
 		return [
 
 				"config"=>[
 					"method"=>"POST",
 					"Action"=>"",
-					"Submit"=>"Post",
+					"Submit"=>"commenter",
 					"class"=>"form_register"
 				],
 				"input"=>[
--- www/Views/post_view.php
+++ www/Views/post_view.php
@@ -21,3 +21,44 @@
         </tbody>
         <?php }} ?>
 </div>
+
+
+
+<?php if(!empty($formErrors)):?>
+	<?php foreach($formErrors as $error):?>
+		<li><?= $error ;?>
+	<?php endforeach;?>
+<?php endif;?>
+
+<div class="commentaires">
+	<h1>ajouté un commentaire</h1>
+	<?php  App\Core\Form::showForm($form); ?>
+</div>
+
+
+
+<div>
+    <table id='tab'>
+        <thead>
+            <tr>
+                <td>id</td>
+                <td>id_article</td>
+                <td>id_user</td>
+                <td>content</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+              
+                foreach($allCommentaire as $Commentairet){
+                if($post["id"]==$_GET["id_article"]){
+                ?>
+                <tr>
+                    <td><?php echo $post["id"]?></td>
+                    <td><?php echo($post["id_article"]);?></td>
+                    <td><?php echo($post["id_user"]);?></td>
+                    <td><?php echo($post["content"]);?></td>
+                </tr>
+        </tbody>
+        <?php }} ?>
+</div>
