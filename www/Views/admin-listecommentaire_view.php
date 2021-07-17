40f84b56dcaa612ac225e711f4978bfbfe2c7791
Chaochao <chaochao.zhou@hotmail.com>
2021-07-17 18:48:53 +0200

    Redirection single post

--- www/Views/admin-listecommentaire_view.php
+++ www/Views/admin-listecommentaire_view.php
@@ -33,7 +33,7 @@
                     <td><?php echo($Commentaire["id_article"]);?></td>
                     <td><?php echo($Commentaire["id_user"]);?></td>
                     <td><?php echo($Commentaire["content"]);?></td>
-                    <td><a href="\admin\commentaire?id=<?php echo $Commentaire["id"];?>">affichier</a> <a href="\admin\delete-commentaire?id=<?php echo $Commentaire["id"];?>">supprimer</a> <a href="\admin\edit-commentaire?id=<?php echo $Commentaire["id"];?>"> editer<a>
+                    <td><a href="\post?id=<?php echo $Commentaire["id_article"];?>">affichier</a> <a href="\admin\delete-commentaire?id=<?php echo $Commentaire["id"];?>">supprimer</a> <a href="\admin\edit-commentaire?id=<?php echo $Commentaire["id"];?>"> editer<a>
                 </tr>
                 <?php 
             }
