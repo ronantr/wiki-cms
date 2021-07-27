556d58c90820aafae7c91a2cb53c728cca8b48d3
Alexis LOURS <alexislours@protonmail.com>
2021-07-27 14:06:16 +0200

    feat: fix installer title

--- www/Views/Templates/installer_tpl.php
+++ www/Views/Templates/installer_tpl.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<title><?php echo $title ?></title>
+		<title>Installeur</title>
 		<meta name="description" content="ceci est la page de template">
 		<link rel=stylesheet href=<?php echo App\Core\View::lookupfile('main.css'); ?> >
 		<script type="text/javascript" src=<?php echo App\Core\View::lookupfile('main.js'); ?>></script>
