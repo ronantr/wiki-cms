c76a6fe3f37f251ae403630bf018bc7eee5a4fd6
Alexis LOURS <alexislours@protonmail.com>
2021-07-28 14:34:51 +0200

    fix formatting

--- www/Views/admin/add-post_view.php
+++ www/Views/admin/add-post_view.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <script src="https://cdn.tiny.cloud/1/eo59fq7w6j1puvdqjeu1rpwttfb7zmw1xt5pz6cxsykca6l9/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
@@ -18,19 +19,20 @@
       toolbar_mode: 'floating',
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Author name',
-   });
+    });
   </script>
 </head>
+
 <body>
 
-<h2>Nouveau POST</h2>
+  <h2>Nouveau POST</h2>
 
-<?php if(!empty($formErrors)):?>
-	<?php foreach($formErrors as $error):?>
-		<li><?= $error ;?>
-	<?php endforeach;?>
-<?php endif;?>
-<div class="flex-col">
-<?php  App\Core\Form::showForm($form); ?>
-</div>
+  <?php if (!empty($formErrors)) : ?>
+    <?php foreach ($formErrors as $error) : ?>
+      <li><?= $error; ?>
+      <?php endforeach; ?>
+    <?php endif; ?>
+    <div class="flex-col">
+      <?php App\Core\Form::showForm($form); ?>
+    </div>
 </body>
