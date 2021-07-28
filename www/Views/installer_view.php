2199081ffd5961d6beeb13e00cf4db399b6ef872
Alexis LOURS <alexislours@protonmail.com>
2021-07-28 13:55:22 +0200

    feat: require fields

--- www/Views/installer_view.php
+++ www/Views/installer_view.php
@@ -22,23 +22,23 @@
         <form action="/make-install" method="post" class="form">
             <h1>Installeur</h1>
             <label for="DBHOST">DBHOST:</label><br>
-            <input class="form-input" type="text" id="DBHOST" name="data[DBHOST]" placeholder="Hote de la BDD"><br>
+            <input required class="form-input" type="text" id="DBHOST" name="data[DBHOST]" placeholder="Hote de la BDD"><br>
             <label for="DBNAME">DBNAME:</label><br>
-            <input class="form-input" type="text" id="DBNAME" name="data[DBNAME]" placeholder="Nom de la BDD"><br>
+            <input required class="form-input" type="text" id="DBNAME" name="data[DBNAME]" placeholder="Nom de la BDD"><br>
             <label for="DBPREFIX">DBPREFIX:</label><br>
-            <input class="form-input" type="text" id="DBPREFIX" name="data[DBPREFIX]" placeholder="Prefix de la BDD"><br>
+            <input required class="form-input" type="text" id="DBPREFIX" name="data[DBPREFIX]" placeholder="Prefix de la BDD"><br>
             <label for="DBUSER">DBUSER:</label><br>
-            <input class="form-input" type="text" id="DBUSER" name="data[DBUSER]" placeholder="User BDD"><br>
+            <input required class="form-input" type="text" id="DBUSER" name="data[DBUSER]" placeholder="User BDD"><br>
             <label for="DBPWD">DBPWD:</label><br>
-            <input class="form-input" type="password" id="DBPWD" name="data[DBPWD]" placeholder="MDP BDD"><br>
+            <input required class="form-input" type="password" id="DBPWD" name="data[DBPWD]" placeholder="MDP BDD"><br>
             <label for="DBPORT">DBPORT:</label><br>
-            <input class="form-input" type="text" id="DBPORT" name="data[DBPORT]" placeholder="Port BDD"><br>
+            <input required class="form-input" type="text" id="DBPORT" name="data[DBPORT]" placeholder="Port BDD"><br>
             <label for="Username">Username:</label><br>
-            <input class="form-input" type="text" id="Username" name="user[Username]" placeholder="Admin"><br>
+            <input required class="form-input" type="text" id="Username" name="user[Username]" placeholder="Admin"><br>
             <label for="Email">Email:</label><br>
-            <input class="form-input" type="email" id="Email" name="user[Email]" placeholder="Email Admin"><br>
+            <input required class="form-input" type="email" id="Email" name="user[Email]" placeholder="Email Admin"><br>
             <label for="Password">Password:</label><br>
-            <input class="form-input" minlength="8" type="password" id="Password" name="user[Password]" placeholder="MDP Admin"><br>
+            <input required class="form-input" minlength="8" type="password" id="Password" name="user[Password]" placeholder="MDP Admin"><br>
             <!-- <input type="hidden" id="IsDeleted" name="user[IsDeleted]" value="0"><br>
   <input type="hidden" id="Role" name="user[Role]" value="1"><br> -->
             <input type="submit" class="form-submit" value="Submit">
