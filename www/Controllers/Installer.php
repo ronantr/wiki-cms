572ed6ddfb226fcf31dc06c52dcc275b24a733c9
ronan <50153930+ronantr@users.noreply.github.com>
2021-07-28 12:43:23 +0200

    Update Installer.php

--- www/Controllers/Installer.php
+++ www/Controllers/Installer.php
@@ -12,7 +12,7 @@ class Installer
 {
     public function initialisationAction(){
         $installer = new CoreInstaller();
-        if($installer->checkEnvExist() == false) {
+        if($installer->checkEnvExist() == 'false') {
             
             $view = new View("installer", "installer");
             $view->assign("title","Installer");
