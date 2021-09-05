dfd0558864294ab6b6656c9650bae2c44dc759c8
ronantr <noob93500@gmail.com>
2021-09-05 14:02:30 +0200

    modifi view user

--- www/Views/admin/admin-user_view.php
+++ www/Views/admin/admin-user_view.php
@@ -3,9 +3,9 @@
 Id User : <?php echo $user['id']; ?>
     </br>Username : <?php echo $user['username']; ?>
     </br>Email : <?php echo $user['email']; ?>
-    </br>Rôle : <?php echo $user['role']; ?>
-    </br>Compte Validé : <?php echo $user['emailVerified']; ?>
+    </br>Rôle : <?php  switch($user['role']){case 1: echo "Administrateur"; break; case 2: echo "Abonné"; break; case 3: echo "Editor"; break;} ?>
+    </br>Compte Validé : <?php echo($user['emailVerified'] != '1' ) ? 'Non Verifier' : 'Verifier' ;?>
     </br></br>
-    <?php var_dump($_SESSION);?>
+    <?php if($user['emailVerified'] != '1' ) {?>
     <button><a href='/admin/MailVerif'class='button-valide'> verfication email </a></button>
-<?php } } ?>
+<?php } }} ?>
