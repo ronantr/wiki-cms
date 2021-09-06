56fedef53fa02266048babae26b44ab96f62db91
ronantr <noob93500@gmail.com>
2021-09-06 03:38:02 +0200

    Update theme_view.php

--- www/Views/admin/theme_view.php
+++ www/Views/admin/theme_view.php
@@ -1,10 +1,9 @@
 <label for='template'>choose template</label>
-<?php foreach($themes as $theme){ ?>
+
 <form action="/admin/theme/edit"method="POST">
 <select name="templates" id="templates">
-    <option value=1 <?php if($theme['id'] == 1){ echo "Selected";}?> >white</option>
-    <option value=2 <?php if($theme['id'] == 2){ echo "Selected";}?> >black</option>
-    <?php } ?>
+    <option value=1 >white</option>
+    <option value=2 >black</option>
 </select>
 
 <input type="submit" value="Submit">
